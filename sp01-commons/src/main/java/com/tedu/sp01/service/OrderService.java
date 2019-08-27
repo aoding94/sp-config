3ffce98d470bdd21324137e9c4d87652e656434a
Administrator <Administrator@SC-201709151629>
2019-08-27 14:48:26 +0800

    commons

---
+++ sp01-commons/src/main/java/com/tedu/sp01/service/OrderService.java
@@ -0,0 +1,12 @@
+package com.tedu.sp01.service;
+
+import com.tedu.sp01.pojo.Order;
+
+public interface OrderService {
+	
+	Order getOrder(String OrderId);
+	/**
+	 * ±£´æ¶©µ¥
+	 */
+	void addOrder(Order order);
+}
